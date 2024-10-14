7a6bca14d7bea541cbd0b5f02652039a3f4811ef
elitamsut <e.tamsut@gmail.com>
2024-10-14 23:04:19 +0300

    Add https to reference NS

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
     return 'Hello, World From Reference!'
     # This is  Eli migdal task comment for testing purposes final
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8080)
 
