0f61f0e8887aff3d479dd81ced50ac6ab8f3064a
elitamsut <e.tamsut@gmail.com>
2024-10-14 23:46:36 +0300

    Add https to test  NS

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    ## Use your wildcard SSL certificate and key
+    ### Use your wildcard SSL certificate and key
     app.run(host='0.0.0.0', port=8080, ssl_context=('wildcard-cert.pem', 'wildcard-key.pem'))
 
