8c76b6482635a719f50309565cd7c50c2012cf77
elitamsut <e.tamsut@gmail.com>
2024-10-14 23:48:41 +0300

    Add https to test  NS

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World From Reference!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     # Use your wildcard SSL certificate and key
--- tests/app.py
+++ tests/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from Reference!'
+    return 'Hello, World!'
     # This is  Eli migdal version 18 testing   task comment for testing purposes final
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8080)
 
