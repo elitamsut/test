e16d99ffa73cebd2bce4c1668b7b4499cd38b686
elitamsut <e.tamsut@gmail.com>
2024-10-15 16:05:10 +0300

    pytest conflict

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,5 +15,4 @@ def test_hello_world(client):
     """Test the hello world route."""
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, World from Reference!'
-
+    assert response.data == b'Hello, World!'
